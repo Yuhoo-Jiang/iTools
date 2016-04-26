633591dc68101e72586536f73bf6ef5a6d8209b1
Yuhoo <zubing.jiang@hotmail.com>
2016-04-26 10:48:47 +0800

    Initial commit

---
+++ BaseTool/src/main/java/com/itools/string/iStringUtils.java
@@ -0,0 +1,12 @@
+package com.itools.string;
+
+/**
+ * 描述：该工具类主要是提供针对string对象的一些常用操作方法
+ *
+ * Created by Jiang zubing on 2016/4/26.
+ *
+ * ***************更新历史*******************
+ */
+public class iStringUtils {
+
+}
