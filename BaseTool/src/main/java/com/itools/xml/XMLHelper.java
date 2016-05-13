46ef37d61a2c01bd6e768798e73bba5923ccc803
Yuhoo <zubing.jiang@hotmail.com>
2016-05-13 17:51:32 +0800

    增加文件处理工具类

--- BaseTool/src/main/java/com/itools/xml/XMLHelper.java
+++ BaseTool/src/main/java/com/itools/xml/XMLHelper.java
@@ -1,9 +1,10 @@
 package com.itools.xml;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
